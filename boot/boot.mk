1b2ef2ffa8e68de12ed5eb8084f78fd44357f741
linliangjun <me@linliangjun.cn>
2024-12-10 21:31:47 +0800

    将控制权转移给第二阶段引导代码

--- boot/boot.mk
+++ boot/boot.mk
@@ -1,9 +1,14 @@
-$(BUILD_DIR)/boot/bootsect: boot/bootsect.ld $(BUILD_DIR)/boot/bootsect.o
-	$(LD) -T $< -o $@ $(BUILD_DIR)/boot/bootsect.o
+$(BUILD_DIR)/boot/%: boot/%.ld $(BUILD_DIR)/boot/%.o
+	$(LD) -o $@ -T $^
 
 $(BUILD_DIR)/boot/bootsect.lock: $(BUILD_DIR)/boot/bootsect.bin
 	@$(MAKE) -s $(BUILD_DIR)/aurora.img
 	dd if=$< of=$(BUILD_DIR)/aurora.img conv=notrunc status=none
 	@touch $@
 
-build-boot: $(BUILD_DIR)/boot/bootsect.lock
+$(BUILD_DIR)/boot/stage2.lock: $(BUILD_DIR)/boot/stage2.bin
+	@$(MAKE) -s $(BUILD_DIR)/aurora.img
+	dd if=$< of=$(BUILD_DIR)/aurora.img seek=1 conv=notrunc status=none
+	touch $@
+
+build-boot: $(BUILD_DIR)/boot/bootsect.lock $(BUILD_DIR)/boot/stage2.lock
