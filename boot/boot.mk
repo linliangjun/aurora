a03ded642e6dc20f44aadbe70b43ea7a40ff5ede
linliangjun <me@linliangjun.cn>
2025-03-09 01:56:05 +0800

    将控制权转移给第二阶段引导代码

--- boot/boot.mk
+++ boot/boot.mk
@@ -1,8 +1,27 @@
-$(BUILD_DIR)/boot/bootsect: boot/bootsect.ld $(BUILD_DIR)/boot/bootsect.o
-	$(LD) -T $< -o $@ $(BUILD_DIR)/boot/bootsect.o
+BOOT_ASMFLAGS := -Iboot/include
 
-$(BUILD_DIR)/boot/bootsect.lock: $(BUILD_DIR)/boot/bootsect.bin $(SYSTEM_IMG)
-	dd if=$< of=$(SYSTEM_IMG) conv=notrunc
-	@touch $@
+BOOT_STAGE := 1 2
+BOOT_STAGE_LBA := 0 1
 
-build-boot: $(BUILD_DIR)/boot/bootsect.lock
+$(BUILD_DIR)/boot/%.o: boot/%.S
+	@mkdir -p $(@D)
+	$(CC) -c $(ASMFLAGS) $(BOOT_ASMFLAGS) -o $@ $<
+
+define boot_stage
+BOOT_LOCK += $(BUILD_DIR)/boot/stage$(1)/stage$(1).lock
+
+BOOT_STAGE$(1)_OBJS := \
+	$(patsubst boot/stage$(1)/%.S,$(BUILD_DIR)/boot/stage$(1)/%.o, $(shell find boot/stage$(1) -name "*.S"))
+
+$(BUILD_DIR)/boot/stage$(1)/stage$(1): boot/stage$(1)/stage$(1).ld $$(BOOT_STAGE$(1)_OBJS)
+	$$(LD) -T $$< -o $$@ $$(BOOT_STAGE$(1)_OBJS)
+
+$(BUILD_DIR)/boot/stage$(1)/stage$(1).lock: $(BUILD_DIR)/boot/stage$(1)/stage$(1).bin
+	@$(MAKE) -s $(SYSTEM_IMG)
+	dd if=$$< of=$(SYSTEM_IMG) seek=$(word $(1), $(BOOT_STAGE_LBA)) conv=notrunc
+	@touch $$@
+endef
+
+$(foreach stage, $(BOOT_STAGE), $(eval $(call boot_stage,$(stage))))
+
+build-boot: $(BOOT_LOCK)
---
+++ boot/include/config.h
@@ -0,0 +1,16 @@
+/**
+ * 引导配置
+ *
+ * Copyright 2025 linliangjun
+ */
+
+#ifndef __BOOT_CONFIG_H
+#define __BOOT_CONFIG_H
+
+#define STAGE2_CYLINDER 0       // 第二阶段引导起始柱面号
+#define STAGE2_HEAD 0           // 第二阶段引导起始磁头号
+#define STAGE2_SECTOR 2         // 第二阶段引导起始扇区号
+#define STAGE2_SECTOR_COUNT 15  // 第二阶段引导扇区数量
+#define STAGE2_ADDR 0x7e00      // 第二阶段引导地址
+
+#endif
