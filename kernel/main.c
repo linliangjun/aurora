8b3db254a2ee5087540c202a60f2668514a7515e
linliangjun <me@linliangjun.cn>
2025-05-12 15:41:57 +0800

    :sparkles: feat(kernel): 实现内核打印（printk）

--- kernel/main.c
+++ kernel/main.c
@@ -4,14 +4,14 @@
  * Copyright 2025 linliangjun
  */
 
-#include "tty.h"
+#include "printk.h"
 #include "types.h"
 
 #define KERNEL_VERSION "0.0.1"
 
 __attribute__((noreturn)) void main(void)
 {
-    tty_printf("Kernel version \"%s\" (build %s %s)\n", KERNEL_VERSION, __DATE__, __TIME__);
+    PR_INFO("Kernel version \"%s\" (build %s %s)\n", KERNEL_VERSION, __DATE__, __TIME__);
     while (true)
         ;
 }
